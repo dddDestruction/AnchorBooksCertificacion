b78073fbf60af7487aa9d09f3eb43051e27c8c59
Nicolás Díaz <nifradich@hotmail.com>
2021-01-16 09:33:48 -0300

    commit inicial

---
+++ app/src/main/java/com/example/anchorbookscertificacion/model/AnchorRespository.kt
@@ -0,0 +1,4 @@
+package com.example.anchorbookscertificacion.model
+
+class AnchorRespository {
+}
---
+++ app/src/main/java/com/example/anchorbookscertificacion/model/IAnchorRepository.kt
@@ -0,0 +1,4 @@
+package com.example.anchorbookscertificacion.model
+
+interface IAnchorRepository {
+}
